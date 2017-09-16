f83f2928e1e946b5617c80ab1bdeebac8040021d
hsjung22 <hsjung22@gmail.com>
2017-09-16 11:35:46 -0700

    feat: Add Entry component

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>hsj5</Text>
-        <Ionicons name='ios-paw' color="red" size={100} />
+      <View>
+        <AddEntry />
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
